543421bff9b824626f395ef9f8967614d88e7968
MbilawalRaza <155060676+MbilawalRaza@users.noreply.github.com>
2024-11-11 10:10:18 -0800

    js calculator
    updated

--- app.js
+++ app.js
@@ -1,47 +1,21 @@
-// var input = document.getElementById('inp')
-// console.log(input)
 
-// function getnum(num){
-//     console.log('number', num)
-//     input.value += num
-// }
-
-// function allClear(){
-//     input.value = ""
-// }
-
-// function calc(){
-   
-//     const validExpression = /^[\d+\-*/.]+$/;
-            
-//     if (validExpression.test(input.value)) {
-        
-//        var value = input.value 
-//        input= eval(value)
-//     } else {
-       
-//         input.value = "Error";
-// }}
 
 
 var input = document.getElementById('inp')
 console.log(input)
 
-function setANumber(num){
+function getnum(num){
     console.log('number', num)
     input.value += num
+    console.log('cklick')
 }
 
-function allClear(){
+function Ac(){
     input.value = ""
 }
 
-function deleteLast() {
-    const display = document.getElementById("inp");
-    display.value = display.value.slice(0, -1) || "0";
-}
 
-function calc(){
+function calculate(){
     console.log(input.value)
     var output = eval(input.value)
     console.log(output)
